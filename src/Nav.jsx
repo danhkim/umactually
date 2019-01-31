01b0b6e79c383f575130bb44110c64194ad81005
Phillip Kang <phillipxkang@gmail.com>
2019-01-31 13:34:33 -0800

    check it

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -13,13 +13,29 @@ export default class Nav extends Component {
     return (
       <div className="Nav">
         <div className="0 nav-button actually current" 
-          onClick={event => {this.props.control(0); this.animate(event)}}><a href="#home"/>Um, Actually,</div>
+          onClick={event => {this.props.control(0); this.animate(event)}}>
+          <span className="nav-button-text">
+            Um, <span className="mobile-text"/>Actually,
+          </span>
+        </div>
         <div className="1 nav-button what" 
-          onClick={event => {this.props.control(1); this.animate(event)}}><a href="#home"/>Um, What?</div>
+          onClick={event => {this.props.control(1); this.animate(event)}}>
+          <span className="nav-button-text">
+            Um, <span className="mobile-text"/>What?
+          </span>
+        </div>
         <div className="2 nav-button pics" 
-          onClick={event => {this.props.control(2); this.animate(event)}}><a href="#home"/>Um, Pics?</div>
+          onClick={event => {this.props.control(2); this.animate(event)}}>
+          <span className="nav-button-text">
+            Um, <span className="mobile-text"/>Pics?
+          </span>
+        </div>
         <div className="3 nav-button gimme" 
-          onClick={event => {this.props.control(3); this.animate(event)}}><a href="#home"/>Um, Gimme</div>
+          onClick={event => {this.props.control(3); this.animate(event)}}>
+          <span className="nav-button-text">
+            Um, <span className="mobile-text"/>Gimme
+          </span>
+        </div>
         <div className="nav-highlight" />
       </div>
     );
