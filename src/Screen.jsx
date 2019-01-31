0fd8942759c639269a82aa9cb543146d9fca9274
Phillip Kang <phillipxkang@gmail.com>
2019-01-31 06:53:21 -0800

    the miracle of life

--- src/Screen.jsx
+++ src/Screen.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import image from './test.jpeg';
 import './Screen.css';
 
 export default class Screen extends Component {
@@ -6,7 +7,7 @@ export default class Screen extends Component {
     return (
       <div className="Screen">
         <h1 className="vcenter">{this.props.text}</h1>
-        
+        <a className="download" href={image} download="miracles.jpeg">Download</a>
       </div>
     );
   }
